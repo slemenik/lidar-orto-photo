9c73ac12218732371745804708583ea05c577d56
slemenik <ms4388@student.uni-lj.si>
2018-01-17 12:40:35 +0100

    Convert LAS file to 1.3, show final solution

--- Program.cs
+++ Program.cs
@@ -1,37 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Net.Mime;
-using System.Reflection;
 using laszip.net;
-//using LibLAS;
+
 
 namespace lidar_orto_photo
 {
-    internal class Program
+    internal static class Program
     {
 	    private static readonly String ResourceDirectoryPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\resources\\";
-	    private static readonly String temp_file_name = "GK_470_97.laz";
-	    
-	    
-	    
-        struct Point3D
-		{
-			public double X;
-			public double Y;
-			public double Z;
-		}
+//	    private static readonly String temp_file_name = "GK_470_97.laz";	   
+        
 
-		public static void test3()
-		{
-			WriteLaz(); // Write a file
-			ReadLaz();  // Read it back
-		}
-
-		static string FileName = ResourceDirectoryPath + temp_file_name;
-
-		static void ReadLaz()
+		static void ReadLaz(string fileName)
 		{
 			var lazReader = new laszip_dll();
 			var lazWriter = new laszip_dll();
@@ -39,32 +20,32 @@ namespace lidar_orto_photo
 			var compressed = true;
 			if (err == 0)
 			{
-				lazReader.laszip_open_reader(FileName, ref compressed);
-//				var numberOfPoints = (uint)10;
+				var filePath = ResourceDirectoryPath + fileName;
+				lazReader.laszip_open_reader(filePath, ref compressed);
 				var numberOfPoints = lazReader.header.number_of_point_records;
 	
 				// Check some header values
-				Console.WriteLine(lazReader.header.min_x);
-				Console.WriteLine(lazReader.header.min_y);
-				Console.WriteLine(lazReader.header.min_z);
-				Console.WriteLine(lazReader.header.max_x);
-				Console.WriteLine(lazReader.header.max_y);
-				Console.WriteLine(lazReader.header.max_z);
+//				Console.WriteLine(lazReader.header.min_x);
+//				Console.WriteLine(lazReader.header.min_y);
+//				Console.WriteLine(lazReader.header.min_z);
+//				Console.WriteLine(lazReader.header.max_x);
+//				Console.WriteLine(lazReader.header.max_y);
+//				Console.WriteLine(lazReader.header.max_z);
+//				Console.WriteLine("format: " + lazReader.header.point_data_format);
 	
 				int classification = 0;
 //				var coordArray = new double[3];
 	
-				lazWriter.header.number_of_point_records = numberOfPoints;
+				lazWriter.header.number_of_point_records = n~umberOfPoints;
 
-				// Header Min/Max needs to be set to extents of points
-				lazWriter.header = lazReader.header; // LL Point
+				lazWriter.header = lazReader.header;
 
-				// Open the writer and test for errors
-				err = lazWriter.laszip_open_writer(ResourceDirectoryPath +"temp.laz", true);
+
+				var newFileName = Path.GetFileNameWithoutExtension(fileName) + "_new.laz";
+				err = lazWriter.laszip_open_writer(ResourceDirectoryPath + newFileName, true);
 				if (err == 0)
 				{
-					double[] coordArray = new double[3];
-					for (int pointIndex = 0; pointIndex < numberOfPoints; pointIndex++)
+					for (var pointIndex = 0; pointIndex < numberOfPoints; pointIndex++)
 					{
 						lazReader.laszip_read_point();
 
@@ -74,7 +55,56 @@ namespace lidar_orto_photo
 
 						lazWriter.point = lazReader.point;
 						
+						var rnd = new Random();				
+						var r = rnd.Next(0, 255);
+						var g = rnd.Next(0, 255);
+						var b = rnd.Next(0, 255);
+//						
+						lazWriter.point.rgb = new [] {(ushort) (r << 8), (ushort) (g << 8),(ushort) (b << 8), (ushort) 0};
+
+						var waveformPacket = lazReader.point.wave_packet;
+
+						var xt = 123;
+						var yt = 2444;
+						var zt = 132122;
+////
+						waveformPacket[17] = (byte)(xt >> 24);
+						waveformPacket[18] = (byte)(xt >> 16);
+						waveformPacket[19] = (byte)(xt >> 8);
+						waveformPacket[20] = (byte) xt;
 						
+						waveformPacket[21] = (byte)(yt >> 24);
+						waveformPacket[22] = (byte)(yt >> 16);
+						waveformPacket[23] = (byte)(yt >> 8);
+						waveformPacket[24] = (byte) yt;
+						
+						waveformPacket[25] = (byte)(zt >> 24);
+						waveformPacket[26] = (byte)(zt >> 16);
+						waveformPacket[27] = (byte)(zt >> 8);
+						waveformPacket[28] = (byte) zt;
+						
+						if (classification == 0)
+						{
+//							Console.WriteLine(lazReader.point.rgb[0]);
+//							Console.WriteLine(lazReader.point.rgb[1]);
+//							Console.WriteLine(lazReader.point.rgb[2]);
+//							Console.WriteLine(lazReader.point.rgb[3]);
+//							
+//							Console.WriteLine((int) lazReader.point.rgb[0] >> 8);
+//							Console.WriteLine((int) lazReader.point.rgb[1] >> 8);
+//							Console.WriteLine((int) lazReader.point.rgb[2] >> 8);
+//							Console.WriteLine((int) lazReader.point.rgb[3] >> 8);
+//							Console.WriteLine(lazReader.point.rgb[3]);
+							
+							Console.WriteLine(lazReader.point.wave_packet);
+							var a = lazReader.point.wave_packet;
+							for (int i = 0; i < a.Length; i++)
+							{
+								Console.Write(a[i] + "  ");
+							}
+//							break;
+							classification++;
+						}
 //						
 						err = lazWriter.laszip_write_point();
 						if (err != 0) break;
@@ -92,129 +122,55 @@ namespace lidar_orto_photo
 			}
 		}
 
-		private static void WriteLaz()
-		{
-			// --- Write Example
-			var point = new Point3D();
-			var points = new List<Point3D>();
+		
 
-			point.X = 1000.0;
-			point.Y = 2000.0;
-			point.Z = 100.0;
-			
-			points.Add(point);
-
-			point.X = 5000.0;
-			point.Y = 6000.0;
-			point.Z = 200.0;
-			points.Add(point);
-
-			var lazWriter = new laszip_dll();
-			var err = lazWriter.laszip_clean();
-			if (err == 0)
-			{
-				// Number of point records needs to be set
-				lazWriter.header.number_of_point_records = (uint)points.Count;
-
-				// Header Min/Max needs to be set to extents of points
-				lazWriter.header.min_x = points[0].X; // LL Point
-				lazWriter.header.min_y = points[0].Y;
-				lazWriter.header.min_z = points[0].Z;
-				lazWriter.header.max_x = points[1].X; // UR Point
-				lazWriter.header.max_y = points[1].Y;
-				lazWriter.header.max_z = points[1].Z;
-
-				// Open the writer and test for errors
-				err = lazWriter.laszip_open_writer(FileName, true);
-				if (err == 0)
-				{
-					double[] coordArray = new double[3];
-					foreach (var p in points)
-					{
-						coordArray[0] = p.X;
-						coordArray[1] = p.Y;
-						coordArray[2] = p.Z;
-
-						// Set the coordinates in the lazWriter object
-						lazWriter.laszip_set_coordinates(coordArray);
-
-						// Set the classification to ground
-						lazWriter.point.classification = 2;
-						
-//						new laszip_point().
-						// Write the point to the file
-						err = lazWriter.laszip_write_point();
-						if (err != 0) break;
-					}
-
-					// Close the writer to release the file (OS lock)
-					err = lazWriter.laszip_close_writer();
-					lazWriter = null;
-				}
-			}
-
-			if (err != 0)
-			{
-				// Show last error that occurred
-				Debug.WriteLine(lazWriter.laszip_get_error());
-			}
-			// --- Upon completion, file should be 389 bytes
-}
-
-	    private static void runLasViewer()
+	    private static void runLasview(string fileName)
 	    {
-		    ProcessStartInfo start = new ProcessStartInfo();
-		    start.Arguments = "-i \"" + ResourceDirectoryPath + "temp.laz" + "\""; 
-		    start.FileName = ResourceDirectoryPath + "lasview";
-		    start.WindowStyle = ProcessWindowStyle.Normal;
-		    start.CreateNoWindow = false;
-			Process.Start(start);
+		    Console.Write("[{0:h:mm:ss}] Opening lasview.exe...");
+		    var start = new ProcessStartInfo
+		    {
+			    Arguments = "-i \"" + ResourceDirectoryPath + fileName + "\"",
+			    FileName = ResourceDirectoryPath + "lasview",
+			    WindowStyle = ProcessWindowStyle.Normal,
+			    CreateNoWindow = false
+		    };
+
+		    var process = Process.Start(start);
+		    process.WaitForExit();
+		    Console.WriteLine("[DONE]");
+	    }
+
+	    private static void runLas2las(string fileName)
+	    {
+		    Console.Write("[{0:h:mm:ss}] Converting to LAS 1.3 ...", DateTime.Now);
+		    var start = new ProcessStartInfo
+		    {
+			    Arguments = "-i \"" + ResourceDirectoryPath + fileName + "\" -set_point_type 5 -set_version 1.3 -olaz",
+			    FileName = ResourceDirectoryPath + "las2las",
+			    WindowStyle = ProcessWindowStyle.Hidden,
+			    CreateNoWindow = false
+		    };
+		    var process = Process.Start(start);
 		    
+//		    var process = Process.Start(ResourceDirectoryPath+"las2las", 
+//			    "-i \"" + ResourceDirectoryPath+ temp_file_name
+//			    +"\" -set_point_type 5 -set_version 1.3 -odir \""
+//			    +ResourceDirectoryPath.Substring(0,ResourceDirectoryPath.Length-1) + "\" -o \"plis.laz\"");
+		    process.WaitForExit();
+		    Console.WriteLine("[DONE]");
 	    }
 	    
-        public static void Main(string[] args)
+        public static void Main()
         {
-	        Console.WriteLine("start");
-            
-//	        runLasViewer();
-	        ReadLaz();
-	        runLasViewer();
+	        Console.WriteLine("[{0:h:mm:ss}] Start program. ", DateTime.Now);
+	        var fileName = "GK_470_97";
 	        
+//	        runLas2las(fileName + ".laz");
+//	        ReadLaz(fileName + "_1.laz");
+//	        runLasview(fileName + "_1_new.laz");
 	        
-	        Console.WriteLine("end");
+	        Console.WriteLine("[{0:h:mm:ss}] End program.", DateTime.Now);
         }//end main
-
-	    public static void test()
-	    {
-		    var totalPointCount = 0L;
-		    var filename = ResourceDirectoryPath + temp_file_name;
-		    File.OpenRead(filename);
-		    var info = LASZip.Parser.ReadInfo(filename);
-		    totalPointCount += info.Count;
-		    var a = true;
-		    Console.WriteLine($"{Path.GetFileName(filename),-40}{info.Count,20:N0}");
-		    foreach (var ps in LASZip.Parser.ReadPoints(filename, 1))
-		    {
-//                    bounds2.ExtendBy(new Box3d(ps.Positions));
-//                    Console.WriteLine($"  chunk {ps.Count,20:N0}");
-//                laszip_dll.laszip_create().point
-//	            if (a)
-//	            {
-//		            a = false;
-//		            if (ps.Classifications[0] != 4) Console.WriteLine(ps.Classifications[0]);
-			    if (ps.Colors[0][0] != 0 )Console.WriteLine(ps.Colors[0]);
-//	            if (ps.Positions[0] != 4)Console.WriteLine(ps.Positions[0]);
-//		            Console.WriteLine(ps.Classifications);
-//		            Console.WriteLine(ps.Classifications);
-			    
-			    //17,21,25 biti xt
-//	            }
-		    }
-            
-		    Console.WriteLine($"total point count: {totalPointCount:N0}");
-            
-//	        
-		    
-	    }
+	    
     } //end class
 }
